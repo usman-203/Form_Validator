4fdf3c2c7dcb153457e18f00ba3e102d065feddc
usman-203 <62180188+usman-203@users.noreply.github.com>
2020-04-26 08:28:24 -0700

    commit6

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ form.addEventListener("submit", function (e) {
     username.style.borderColor = "#2ecc71";
     document.getElementById("usernameError").innerHTML = "";
   }
+  if (email.value === "") {
+    document.getElementById("emailError").style.visibility = "visible";
+    document.getElementById("emailError").innerHTML = "Email is requirerd";
+    email.style.borderColor = "#e74c32";
+  } else {
+    email.style.borderColor = "#2ecc71";
+    document.getElementById("emailError").innerHTML = "";
+  }
+  if (password.value === "") {
+    document.getElementById("passwordError").style.visibility = "visible";
+    document.getElementById("passwordError").innerHTML =
+      "Password is requirerd";
+    password.style.borderColor = "#e74c32";
+  } else {
+    password.style.borderColor = "#2ecc71";
+    document.getElementById("passwordError").innerHTML = "";
+  }
+  if (password2.value === "") {
+    document.getElementById("password2Error").style.visibility = "visible";
+    document.getElementById("password2Error").innerHTML =
+      "Confirm Password is requirerd";
+    password2.style.borderColor = "#e74c32";
+  } else if (password.value == password2.value) {
+    document.getElementById("password2Error").innerHTML = "";
+    password2.style.borderColor = "#2ecc71";
+  } else {
+    document.getElementById("password2Error").style.visibility = "visible";
+    document.getElementById("password2Error").innerHTML =
+      "Password is not match";
+    password2.style.borderColor = "#e74c32";
+  }
+});
